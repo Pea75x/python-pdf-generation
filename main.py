11d6da811c347f195fcc71e539c05712f5fc1f02
Priya <priya_93_x@hotmail.co.uk>
2024-08-30 11:39:30 +0100

    added table, company logo

--- main.py
+++ main.py
@@ -19,7 +19,40 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice no. {invoice_no}", ln=1)
     # date
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Date {date}")
+    pdf.cell(w=50, h=8, txt=f"Date {date}", ln=1)
+    # data table header
+    columns = df.columns
+    columns = [item.replace("_", " ").title() for item in columns]
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=70, h=8, txt=columns[1], border=1)
+    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=30, h=8, txt=columns[3], border=1)
+    pdf.cell(w=20, h=8, txt=columns[4], border=1, ln=1)
+    # data table rows
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=10)
+        pdf.set_text_color(80, 80, 80)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=20, h=8, txt=str(row["total_price"]), border=1, ln=1)
+
+    total_sum = df["total_price"].sum()
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=70, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=20, h=8, txt=str(total_sum), border=1, ln=1)
+
+    # Total sum sentence
+    pdf.set_font(family="Times", size=10)
+    pdf.cell(w=30, h=8, txt=f"The total price is {total_sum}", ln=1)
+
+    # Company name and logo
+    pdf.cell(w=30, h=8, txt="PythonHow", ln=1)
+    pdf.image("pythonhow.png", w=10)
 
     pdf.output(f"pdfs/{filename}.pdf")
 
