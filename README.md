a55eca7f0d93c959417764ba6605cd4b61109d7e
Priya <priya_93_x@hotmail.co.uk>
2024-09-02 12:34:51 +0100

    added to readme

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ Goals
 - Read each xlsx file in the invoices directory
 - Create a PDF for each xlsx file
 - Print data in a table in each PDF
-- Store each pdf document in the PDF directory
+- Store each pdf document in the PDFs directory
+
+![](pdf-preview.png)
+
+![](folder-preview.png)
--- main.py
+++ main.py
@@ -24,25 +24,25 @@ for filepath in filepaths:
     columns = df.columns
     columns = [item.replace("_", " ").title() for item in columns]
     pdf.set_font(family="Times", size=10, style="B")
-    pdf.cell(w=30, h=8, txt=columns[0], border=1)
+    pdf.cell(w=25, h=8, txt=columns[0], border=1)
     pdf.cell(w=70, h=8, txt=columns[1], border=1)
-    pdf.cell(w=30, h=8, txt=columns[2], border=1)
+    pdf.cell(w=35, h=8, txt=columns[2], border=1)
     pdf.cell(w=30, h=8, txt=columns[3], border=1)
     pdf.cell(w=20, h=8, txt=columns[4], border=1, ln=1)
     # data table rows
     for index, row in df.iterrows():
         pdf.set_font(family="Times", size=10)
         pdf.set_text_color(80, 80, 80)
-        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=25, h=8, txt=str(row["product_id"]), border=1)
         pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
-        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border=1)
+        pdf.cell(w=35, h=8, txt=str(row["amount_purchased"]), border=1)
         pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
         pdf.cell(w=20, h=8, txt=str(row["total_price"]), border=1, ln=1)
 
     total_sum = df["total_price"].sum()
-    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=25, h=8, txt="", border=1)
     pdf.cell(w=70, h=8, txt="", border=1)
-    pdf.cell(w=30, h=8, txt="", border=1)
+    pdf.cell(w=35, h=8, txt="", border=1)
     pdf.cell(w=30, h=8, txt="", border=1)
     pdf.cell(w=20, h=8, txt=str(total_sum), border=1, ln=1)
 
